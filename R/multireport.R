522322e331feb827c3a1b7892fff314f0593a622
David Keyes <dgkeyes@gmail.com>
2020-06-19 13:07:22 -0700

    Add report_title_param and report_title_to_lower arguments

--- R/multireport.R
+++ R/multireport.R
@@ -4,10 +4,12 @@
 #'
 #' @param rmarkdown_file The RMarkdown file to be rendered
 #' @param params_data_frame A data frame with all of the parameter options (each variable name is the name of a parameter and all of the rows in that column are the parameter options)
-#' @param report_format The format of the report (html_document, pdf_document, word_document, etc)
+#' @param report_title_param The name (in quotes) or column number of the variable to be used to create report titles
 #' @param report_prefix Text to be put before the file names of rendered reports
-#' @param report_suffix Text to be put fterfile names of rendered reports
-#' @param report_output_dir The name of the directory where the reports will be rendered to
+#' @param report_title_to_lower TRUE/FALSE (FALSE by default) whether you want the report file names to be lower case (e.g. alabama-report.html vs Alabama-Report.html)
+#' @param report_format The format of the report (html_document, pdf_document, word_document, etc)
+#' @param report_output_directory
+#' @param report_suffix Text to be put after names of rendered reports
 #'
 #' @return
 #' @export
@@ -15,19 +17,31 @@
 #' @examples
 multireport <- function(rmarkdown_file,
                         params_data_frame,
-                        report_format = "html_document",
+                        report_title_param = 1,
                         report_prefix = "",
                         report_suffix = "",
+                        report_title_to_lower = FALSE,
+                        report_format = "html_document",
                         report_output_directory = here::here()) {
 
   reports <- tibble::tibble(
     output_format = report_format,
-    output_file = stringr::str_glue("{report_prefix}{stringr::str_to_lower(params_data_frame[[1]])}{report_suffix}"),
+    output_file = stringr::str_glue("{report_prefix}{params_data_frame[[report_title_param]]}{report_suffix}"),
     output_dir = report_output_directory,
     params = purrr::pmap(params_data_frame,
                          list))
+
+  if (report_title_to_lower == TRUE) {
+    reports <- reports %>%
+      mutate(output_file = stringr::str_to_lower(output_file))
+  }
+
+  # return(reports$output_file)
 
   purrr::pwalk(reports,
                rmarkdown::render,
                input = rmarkdown_file)
 }
+
+
+
--- inst/render.R
+++ inst/render.R
@@ -1,9 +1,16 @@
 library(multireport)
 library(tidyverse)
+library(fs)
 
-multireport(rmarkdown_file = "inst/report.Rmd",
-            param_all_options = fivethirtyeight::bad_drivers$state,
-            param_name = "state",
+parameters <- tibble(
+  state = unique(fivethirtyeight::bad_drivers$state),
+  losses = unique(fivethirtyeight::bad_drivers$losses)
+)
+
+multireport(rmarkdown = "inst/report.Rmd",
+            params_data_frame = parameters,
+            report_title_param = "state",
+            report_suffix = "-report",
+            report_title_to_lower = TRUE,
             report_format = "html_document",
-            report_suffix = "report",
-            report_output_dir = "inst")
+            report_output_directory = "inst/reports/")
